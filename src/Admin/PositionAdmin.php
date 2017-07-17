31e546a4c53d1c6316dc4980c753805148db88df
PapsOu <vivien.frasca@libre-informatique.fr>
2017-07-17 13:36:06 +0200

    Add trait HasCircle

--- src/Admin/PositionAdmin.php
+++ src/Admin/PositionAdmin.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * This file is part of the Blast Project package.
+ * This file is part of the Lisem Project.
  *
  * Copyright (C) 2015-2017 Libre Informatique
  *
- * This file is licenced under the GNU LGPL v3.
+ * This file is licenced under the GNU GPL v3.
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
@@ -34,7 +34,7 @@ class PositionAdmin extends CoreAdmin
             return;
         }
 
-        $search = '%'.$value['value'].'%';
+        $search = '%' . $value['value'] . '%';
         $queryBuilder
             ->andWhere($queryBuilder->expr()->orX(
                 $queryBuilder->expr()->like("$alias.firstname", ':firstname'),
@@ -49,13 +49,13 @@ class PositionAdmin extends CoreAdmin
 
     public static function individualsCallback($admin, $property, $value)
     {
-        $searchIndex = $admin->getClass().'SearchIndex';
+        $searchIndex = $admin->getClass() . 'SearchIndex';
         $datagrid = $admin->getDatagrid();
         $queryBuilder = $datagrid->getQuery();
         $alias = $queryBuilder->getRootalias();
 
         $queryBuilder
-            ->leftJoin($searchIndex, 's', 'WITH', $alias.'.id = s.object')
+            ->leftJoin($searchIndex, 's', 'WITH', $alias . '.id = s.object')
             ->where('s.keyword LIKE :value')
             ->andWhere("$alias.isIndividual = true")
             ->setParameter('value', "%$value%")
@@ -64,13 +64,13 @@ class PositionAdmin extends CoreAdmin
 
     public static function organizationsCallback($admin, $property, $value)
     {
-        $searchIndex = $admin->getClass().'SearchIndex';
+        $searchIndex = $admin->getClass() . 'SearchIndex';
         $datagrid = $admin->getDatagrid();
         $queryBuilder = $datagrid->getQuery();
         $alias = $queryBuilder->getRootalias();
 
         $queryBuilder
-            ->leftJoin($searchIndex, 's', 'WITH', $alias.'.id = s.object')
+            ->leftJoin($searchIndex, 's', 'WITH', $alias . '.id = s.object')
             ->where('s.keyword LIKE :value')
             ->andWhere("$alias.isIndividual = false")
             ->setParameter('value', "%$value%")
