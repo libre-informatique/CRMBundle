7b27e919c61e680185088ec375a57fdaf84ce8c5
François GERBES <francois.gerbes@libre-informatique.fr>
2017-09-14 18:54:16 +0200

    Style fix

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -20,7 +20,7 @@ use Blast\OuterExtensionBundle\Entity\Traits\OuterExtensible;
 
 /**
  * Address.
- * https://github.com/Sylius/Sylius/issues/8345 & https://ocramius.github.io/blog/fluent-interfaces-are-evil/
+ * https://github.com/Sylius/Sylius/issues/8345 & https://ocramius.github.io/blog/fluent-interfaces-are-evil/.
  */
 class Address implements AddressInterface, VCardableInterface
 {
@@ -119,7 +119,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set firstName.
      *
      * @param string $firstName
-     *
      */
     public function setFirstName(?string $firstName): void
     {
@@ -156,7 +155,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set postCode.
      *
      * @param string $postCode
-     *
      */
     public function setPostCode(?string $postCode): void
     {
@@ -177,7 +175,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set street.
      *
      * @param string $street
-     *
      */
     public function setStreet(?string $street): void
     {
@@ -198,7 +195,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set city.
      *
      * @param string $city
-     *
      */
     public function setCity(?string $city): void
     {
@@ -219,7 +215,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set countryCode.
      *
      * @param string $countryCode
-     *
      */
     public function setCountryCode(?string $countryCode = null): void
     {
@@ -240,7 +235,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set provinceCode.
      *
      * @param string $provinceCode
-     *
      */
     public function setProvinceCode(?string $provinceCode = null): void
     {
@@ -261,7 +255,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set provinceName.
      *
      * @param string $provinceName
-     *
      */
     public function setProvinceName(?string $provinceName = null): void
     {
@@ -282,7 +275,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set npai.
      *
      * @param bool $npai
-     *
      */
     public function setNpai($npai)
     {
@@ -298,7 +290,7 @@ class Address implements AddressInterface, VCardableInterface
     {
         return $this->npai;
     }
-    
+
     /**
      * Get npai.
      *
@@ -314,7 +306,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set vcardUid.
      *
      * @param string $vcardUid
-     *
      */
     public function setVcardUid($vcardUid)
     {
@@ -335,7 +326,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set confirmed.
      *
      * @param bool $confirmed
-     *
      */
     public function setConfirmed($confirmed)
     {
@@ -367,7 +357,6 @@ class Address implements AddressInterface, VCardableInterface
      * Set organism.
      *
      * @param \Librinfo\CRMBundle\Entity\Organism $organism
-     *
      */
     public function setOrganism(\Librinfo\CRMBundle\Entity\Organism $organism = null): void
     {
